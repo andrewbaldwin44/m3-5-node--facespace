5684e048480af49e0de0b49c1dbe0f8a930887ad
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-07-08 11:22:30 -0400

    Solution to exercise 3

--- handlers.js
+++ handlers.js
@@ -1,23 +1,42 @@
 function handleHomepage(req, res) {
-  res.render('./pages/homepage', { users: users });
+  res.status(200).render('./pages/homepage', { users: users });
 }
 
-function findUser(id) {
-  return users.find(user => user._id == id);
+function findUser(value, key = '_id') {
+  return users.find(user => user[key] == value);
 }
 
-function handleProfilePage(req, res) {
+function handleProfilePage(req, res, next) {
   const userID = req.params.id;
 
   const currentUser = findUser(userID);
-  currentUser.friends = currentUser.friends.map(friend => {
-    return friend = findUser(friend);
-  });
 
-  res.render('./pages/profile', { user: currentUser });
+  if (currentUser) {
+    currentUser.friends = currentUser.friends.map(friend => {
+      return friend = findUser(friend);
+    });
+
+    res.status(200).render('./pages/profile', { user: currentUser });
+  }
+  else next();
+}
+
+function handleLogin(req, res) {
+  res.render('./pages/login');
+}
+
+function handleName(req, res) {
+  const firstName = req.body.firstName;
+
+  const currentUser = findUser(firstName, 'name');
+
+  if (currentUser) {
+    res.status(200).redirect(`./users/${currentUser._id}`);
+  }
+  else res.status(404).redirect('./login');
 }
 
 const { users } = require('./data/users');
 let currentUser = {};
 
-module.exports = { handleHomepage, handleProfilePage };
+module.exports = { handleHomepage, handleProfilePage, handleLogin, handleName };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 
-const { handleHomepage, handleProfilePage } = require('./handlers.js');
+const { handleHomepage, handleProfilePage, handleLogin, handleName } = require('./handlers.js');
 
 // declare the 404 function
 const handleFourOhFour = (req, res) => {
@@ -22,6 +22,8 @@ express()
   // endpoints
   .get('/', handleHomepage)
   .get('/users/:id', handleProfilePage)
+  .get('/login', handleLogin)
+  .post('/getname', handleName)
 
   // a catchall endpoint that will send the 404 message.
   .get('*', handleFourOhFour)
